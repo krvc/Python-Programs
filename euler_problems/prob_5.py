8e42b909175a81677de3b6b61fe89070889873dc
Rakesh Vidya Chandra <krvc.it@gmail.com>
2013-03-17 02:33:38 +0530

    added fifth problem

---
+++ euler_problems/prob_5.py
@@ -0,0 +1,27 @@
+"""
+
+Problem-5:
+
+2520 is the smallest number that can be divided by each of the numbers from
+1 to 10 without any remainder.
+
+What is the smallest number that is evenly divisible by all of the numbers from
+1 to 20?
+
+"""
+
+def greatcommondiv(x, y):
+    if(y == 0):
+        return x
+    return greatcommondiv(y, x % y)
+
+
+def leastcomnfact(x, y):
+    return y * x / greatcommondiv(x, y)
+
+ans = 2
+
+for i in range(3, 20):
+    ans = leastcomnfact(ans, i)
+
+print ans
