8fefb3a86846b4aa1f24cf56d32de4e60bd7cd7a
Rakesh Vidya Chandra <krvc.it@gmail.com>
2013-03-19 19:15:47 +0530

    added seventh problem

---
+++ euler_problems/prob_7.py
@@ -0,0 +1,43 @@
+"""
+
+problem-7:
+
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see
+that the 6th prime is 13.
+
+What is the 10001st prime number?
+
+"""
+
+max_num = 10001
+count = 1
+number = 3
+
+
+def isprime(n):
+    '''check if integer n is a prime'''
+    # make sure n is a positive integer
+    n = abs(int(n))
+    # 0 and 1 are not primes
+    if n < 2:
+        return False
+    # 2 is the only even prime number
+    if n == 2: 
+        return True    
+    # all other even numbers are not primes
+    if not n & 1: 
+        return False
+    # range starts with 3 and only needs to go up the squareroot of n
+    # for all odd numbers
+    for x in range(3, int(n**0.5)+1, 2):
+        if n % x == 0:
+            return False
+    return True
+    
+while(True):
+    if isprime(number):
+        count+=1
+    if count >= max_num:
+        break
+    number+= 1
+print number
